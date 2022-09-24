ea03153ec2960ec873e4f6f2283436c5336c41d9
Darya Zakhar <daryazakhar.89@gmail.com>
2022-09-24 15:04:11 +0300

    git commit - m "2 commit"

--- bot.py
+++ bot.py
@@ -6,14 +6,29 @@ token = "5529319076:AAGNH5OQPmq7ZHXoGOaiDFbP-gnwb6qyJwY"
 bot = telebot.TeleBot(token)
 
 
+def create_keyboard():
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard_yes = types.InlineKeyboardButton(text="Да", callback_data="Yes")
+    keyboard_no = types.InlineKeyboardButton(text="Нет", callback_data="No")
+    keyboard.add(keyboard_yes, keyboard_no)
+    return keyboard
+
+
 @bot.message_handler(commands=["start"])
 def start(message):
+    markup = create_keyboard()
     bot.send_message(message.chat.id, "Привет, я бот-помощник, помогу тебе выбрать восстановленный недорогой телефон")
-    kb = types.InlineKeyboardMarkup()
-    kb_yes = types.InlineKeyboardButton(text="Да", callback_data="Yes")
-    kb_no = types.InlineKeyboardButton(text="Нет", callback_data="No")
-    kb.add(kb_yes, kb_no)
-    bot.send_message(message.chat.id, " Тебе это интересно?", reply_markup=kb)
+    bot.send_message(message.chat.id, " Тебе это интересно?", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_inline(callback):
+    if callback.data == "Yes":
+        pass
+    elif callback.data == "No":
+        bot.send_message(
+            chat_id=callback.message.chat.id,
+            text="Тогда ничем не могу тебе помочь. Пока")
 
 
 bot.polling(none_stop=True)
